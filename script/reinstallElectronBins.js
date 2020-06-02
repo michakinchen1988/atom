aae2d554bba41d12230b423eed6d3be4ddf031e0
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-06-02 13:13:29 -0400

    script: Run download scripts, not npm install

--- script/reinstallElectronBins.js
+++ script/reinstallElectronBins.js
@@ -1,39 +1,60 @@
 const { spawn } = require('child_process');
+const npmlog = require('npmlog');
 
 const electronVersion = require('./config').appMetadata.electronVersion;
 
 if (process.env.ELECTRON_CUSTOM_VERSION !== electronVersion) {
-  console.info(`Env var "ELECTRON_CUSTOM_VERSION" doesn't match "electronVersion" from Atom's package.json (or isn't set)...\n` +
-    `Setting ELECTRON_CUSTOM_VERSION to match electronVersion from Atom\'s package.json (${electronVersion}).\n` +
-    `Running: 'npm install electron-chromedriver electron-mksnapshot'.\n` +
-    '(Some versions of chromedriver and mksnapshot are very large. Downloading them may take a while.)\n');
+  npmlog.info(
+    `env var ELECTRON_CUSTOM_VERSION is either not set,\n` +
+      `or doesn't match electronVersion in ../package.json.\n` +
+      `(is: "${process.env.ELECTRON_CUSTOM_VERSION}", wanted: "${electronVersion}").\n` +
+      `re-downloading chromedriver and mksnapshot.\n`
+  );
 
   process.env.ELECTRON_CUSTOM_VERSION = electronVersion;
-
   const nodePath = process.env.npm_node_execpath;
-  const npmPath = process.env.npm_execpath;
+  const chromedriverDownloaderPath = require.resolve('electron-chromedriver/download-chromedriver.js');
+  const mksnapshotDownloaderPath = require.resolve('electron-mksnapshot/download-mksnapshot.js');
+  const chromedriverDownload = spawn(nodePath, [chromedriverDownloaderPath]);
+  const mksnapshotDownload = spawn(nodePath, [mksnapshotDownloaderPath]);
+  var exitStatus;
 
-  const npmInstall = spawn(nodePath, [npmPath, 'install', 'electron-chromedriver', 'electron-mksnapshot']);
-
-  npmInstall.stdout.on('data', (data) => {
-    console.log(`${data}`);
+  chromedriverDownload.stdout.on('data', (data) => {
+    npmlog.info(`${data}`);
   });
 
-  npmInstall.stderr.on('data', (data) => {
-    console.error(`${data}`);
+  chromedriverDownload.stderr.on('data', (data) => {
+    npmlog.error(`${data}`);
   });
 
-  npmInstall.on('close', (code) => {
-    var exitStatus;
+  chromedriverDownload.on('close', (code) => {
     if (code === 0) {
       exitStatus = "success";
     } else {
       exitStatus = "error";
     }
 
-    console.log(`Done re-downloading chromedriver and mksnapshot. Status: ${exitStatus}\n`);
+    npmlog.info(`Done re-downloading chromedriver. Status: ${exitStatus}`);
+  });
+
+  mksnapshotDownload.stdout.on('data', (data) => {
+    npmlog.info(`${data}`);
+  });
+
+  mksnapshotDownload.stderr.on('data', (data) => {
+    npmlog.error(`${data}`);
+  });
+
+  mksnapshotDownload.on('close', (code) => {
+    if (code === 0) {
+      exitStatus = "success";
+    } else {
+      exitStatus = "error";
+    }
+
+    npmlog.info(`Done re-downloading mksnapshot. Status: ${exitStatus}`);
   });
 
 } else {
-  console.info('INFO: env var "ELECTRON_CUSTOM_VERSION" was set correctly.\nNo need to reinstall chromedriver and mksnapshot. Skipping.\n');
+  npmlog.info('env var "ELECTRON_CUSTOM_VERSION" was set correctly.\n(No need to re-download chromedriver or mksnapshot). Skipping.\n');
 }

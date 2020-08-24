3da722b6fba72ee577a553a8681f0cac63cef943
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-08-24 12:01:02 -0400

    :bug: Skip notarization when skipping code signing
    Notarization only works if you've already signed your app.
    
    https://developer.apple.com/documentation/xcode/notarizing_macos_software_before_distribution

--- script/lib/notarize-on-mac.js
+++ script/lib/notarize-on-mac.js
@@ -1,6 +1,17 @@
 const notarize = require('electron-notarize').notarize;
 
 module.exports = async function(packagedAppPath) {
+  if (
+    !process.env.ATOM_MAC_CODE_SIGNING_CERT_DOWNLOAD_URL &&
+    !process.env.ATOM_MAC_CODE_SIGNING_CERT_PATH
+  ) {
+    console.log(
+      'Skipping notarization because the ATOM_MAC_CODE_SIGNING_CERT_DOWNLOAD_URL environment variable is not defined'
+        .gray
+    );
+    return;
+  }
+
   const appBundleId = 'com.github.atom';
   const appleId = process.env.AC_USER;
   const appleIdPassword = process.env.AC_PASSWORD;

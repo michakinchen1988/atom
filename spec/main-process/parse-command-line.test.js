03ec2d3089c939fc9da16f3acf07553b0183816b
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-01-10 10:49:55 -0500

    spec: parseCommandLine() ignores macOS -psn_0 flag
    This flag is set by Gatekeeper. Test to make sure we filter it out.
    
    yargs thinks this is a bunch of short flags (-p -s -n -_ -0 -_ ...),
    which means to add boolean "true"s to each supposed flag's array
    of arguments, even adding boolean "true"s
    to what is meant to be the"non-flag argument" array/ ("options._")
    
    That gives our parser TypeErrors; It does string-only operations
    on the elements from the "_" non-flag argument array.
    
    If we filter this input properly, we won't have TypeErrors.

--- spec/main-process/parse-command-line.test.js
+++ spec/main-process/parse-command-line.test.js
@@ -41,4 +41,18 @@ describe('parseCommandLine', () => {
       assert.deepEqual(args.pathsToOpen, []);
     });
   });
+
+  describe('when evil macOS Gatekeeper flag \(\`-psn_0_[6/7digits]\`\) is passed', () => {
+    it('successfully ignores non-string elements in the yargs "_" non-flag argument array', () => {
+      const psnFlag = `-psn_0_${Math.floor(Math.random()*10_000_000)}`
+      const args = parseCommandLine([
+        psnFlag,
+        '/some/path',
+        'atom://test/url',
+        'atom://other/url'
+      ]);
+      assert.deepEqual(args.urlsToOpen, ['atom://test/url', 'atom://other/url']);
+      assert.deepEqual(args.pathsToOpen, ['/some/path']);
+    });
+  });
 });
